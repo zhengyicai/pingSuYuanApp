8d71c43f07980815d80d598a46ba1ab25d800b48
zhengyc <1010172623@qq.com>
2020-12-22 00:12:20 +0800

    create

---
+++ pingHome/js/path.js
@@ -0,0 +1,3 @@
+//var  $requestUrl = "http://localhost:8080/cms/";
+var  $requestUrl = "http://device.kangxiaoshuai.com/cms/";
+var  $imageUrl = "http://device.kangxiaoshuai.com:8000/uploadImages/";
